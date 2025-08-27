6c16ab5b40bd1630e109e20cd4cf04c374699a54
thomaswesley <bragatrabalho@gmail.com>
2025-08-27 11:35:47 -0300

    chore(frontend): ajustes e configurações no chat

--- src/views/apps/chat/index.jsx
+++ src/views/apps/chat/index.jsx
@@ -494,7 +494,7 @@ const ChatWrapper = () => {
       socket.off('disconnected', disconnected)
       socket.off('bot-response', botResponse)
     }
-  }, [socket])
+  }, [])
 
   useEffect(() => {
     //getMessages()  
